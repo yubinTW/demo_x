99bf7296fdf5afe891d467010dff4c42af044f77
silvia <clarinet221@gmail.com>
2021-06-09 13:53:10 +0800

    fix ps

--- frontend/src/routes/productSuites.tsx
+++ frontend/src/routes/productSuites.tsx
@@ -12,15 +12,77 @@ import { confirmDialog } from 'primereact/confirmdialog'
 import { Toast } from 'primereact/toast'
 
 function ProductSuites() {
-  const [nodes, setNodes] = useState([]);
-  const nodeservice = new NodeService();
+  const [nodes, setNodes] = useState([])
+  const [nextPath, setNextPath] = useState<string | "">("")
+  const [updateKey, setUpdateKey] = useState<number | 0>(0)
+  const toast = useRef<any | null>(null)
+  const nodeservice = new NodeService()
 
   useEffect(() => {
-    nodeservice.getTreeTableNodes().then(data => setNodes(data));
+    nodeservice.getTreeTableNodes().then(data => setNodes(data)).catch(e => console.error(e));
   }, []);//eslint-disable-line react-hooks/exhaustive-deps
+
+  async function setUpSelect(apiId: string) {
+    try {
+      const npath = "/api-viewer/";
+      const pathconcat = npath.concat(apiId);
+      await setNextPath(pathconcat);
+      if (updateKey == 1) {
+        confirm()
+      }
+
+    }
+    catch (error) {
+      console.log("setNextPath fail: ", error)
+
+    }
+
+  };
+  async function onSelect(event: any) {
+    //console.log(event)
+    try {
+      if (event.node.data.type === "API") {
+        await setUpSelect(event.node.data.id)
+        if (updateKey == 0) {
+          await setUpSelect(event.node.data.id)
+          setUpdateKey(1)
+        }
+      }
+    }
+    catch (error) {
+      console.log("Select error: ", error)
+    }
+
+  };
+  const onUnselect = (event: any) => {
+    console.log('unselect')
+  }
+  const accept = () => {
+    toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 })
+    window.location.pathname = nextPath
+
+    //return <APIViewer apiId={selectedNodeKey} />;
+  }
+
+  const reject = () => {
+    toast.current.show({ severity: 'info', summary: 'Rejected', detail: 'You have rejected', life: 3000 })
+  }
+  async function confirm() {
+    //await setUpSelect(apiId);
+    await setUpdateKey(0)
+    console.log(nextPath)
+    confirmDialog({
+      message: 'Are you sure you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept,
+      reject
+    })
+  };
 
   return (
     <div>
+      <Toast ref={toast} />
       <div className="p-grid">
         <div className="p-col" />
         <div className="p-col-10">
@@ -30,119 +92,20 @@ function ProductSuites() {
       </div>
       <div className="p-grid">
         <div className="p-col" />
-        <div className="p-col-10 card">
-          <TreeTable value={nodes}>
-            <Column field="name" header="Name" expander></Column>
-            <Column field="id" header="ID"></Column>
-            <Column field="type" header="Type"></Column>
-          </TreeTable>
-          {/* const [nodes, setNodes] = useState([])
-    const [nextPath, setNextPath] = useState<string | "">("")
-    const [updateKey,setUpdateKey] = useState<number | 0>(0)
-    const toast = useRef<any | null>(null)
-    const nodeservice = new NodeService()
-
-    useEffect(() => {
-        nodeservice.getTreeTableNodes().then(data => setNodes(data)).catch(e => console.error(e));
-    }, []);//eslint-disable-line react-hooks/exhaustive-deps
-    
-    async function setUpSelect(apiId:string)
-    {
-      try{
-        const npath = "/api-viewer/";
-        const pathconcat = npath.concat(apiId);
-        await setNextPath(pathconcat);
-        if(updateKey==1)
-        {
-          confirm()
-        }
-        
-      }
-      catch(error)
-      {
-        console.log("setNextPath fail: ",error)
-        
-      }
-      
-    };
-    async function onSelect(event: any) {
-      //console.log(event)
-      try
-      {
-        if(event.node.data.type === "API")
-        {
-          await setUpSelect(event.node.data.id)
-          if(updateKey==0)
-          {
-            await setUpSelect(event.node.data.id)
-            setUpdateKey(1)
-          }
-        }
-      }
-      catch(error)
-      {
-        console.log("Select error: ", error)
-      }
-      
-    };
-    const onUnselect = (event: any) => {
-      console.log('unselect')
-    }
-    const accept =  ()  => {
-      toast.current.show({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 })
-      window.location.pathname = nextPath
-      
-      //return <APIViewer apiId={selectedNodeKey} />;
-    }
-
-    const reject = () => {
-      toast.current.show({ severity: 'info', summary: 'Rejected', detail: 'You have rejected', life: 3000 })
-    }
-    async function confirm(){
-      //await setUpSelect(apiId);
-      await setUpdateKey(0)
-      console.log(nextPath)
-      confirmDialog({
-          message: 'Are you sure you want to proceed?',
-          header: 'Confirmation',
-          icon: 'pi pi-exclamation-triangle',
-          accept,
-          reject
-      })
-    };
-    
-    return (
-        <div>
-          <Toast ref={toast} />
-        <div className="p-grid">
-          <div className="p-col" />
-          <div className="p-col-10">
-            <h5>Hello PS</h5>
+        <div className="p-col-10">
+          <div className="card">
+            <TreeTable value={nodes} selectionMode="single" onSelect={onSelect} onUnselect={onUnselect}>
+              <Column field="name" header="Name" expander></Column>
+              <Column field="id" header="ID"></Column>
+              <Column field="type" header="Type"></Column>
+            </TreeTable>
           </div>
-          <div className="p-col" />
-        </div>
-        <div className="p-grid">
-          <div className="p-col" />
-          <div className="p-col-10">
-            <div className="card">
-              <TreeTable value={nodes} selectionMode="single"  onSelect={onSelect} onUnselect={onUnselect}>
-                  <Column field="name" header="Name" expander></Column>
-                  <Column field="id" header="ID"></Column>
-                  <Column field="type" header="Type"></Column>
-              </TreeTable>
-            </div>
-          </div>
-          <div className="p-col" /> */}
         </div>
         <div className="p-col" />
       </div>
-
+      <div className="p-col" />
     </div>
-  );
+  )
 }
 
-export default ProductSuites;
-  //   )
-  // }
-
-  // export default ProductSuites
+export default ProductSuites
