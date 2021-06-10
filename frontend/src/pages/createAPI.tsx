67f14404f62f6e7c6ce585a2d48b319aa2be0d9c
xiao617 <cxliao@liaoliqideMacBook-Pro.local>
2021-06-10 13:39:51 +0800

    add createapi page

--- frontend/src/pages/createAPI.tsx
+++ frontend/src/pages/createAPI.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+import {Checkbox} from 'primereact/checkbox';
+import {RadioButton} from 'primereact/radiobutton';
+import {Dropdown} from 'primereact/dropdown';
+import {InputTextarea} from 'primereact/inputtextarea';
+
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
 
 function CreateAPI() {
-    
+  const [selectedState, setSelectedState] = useState<any>(null);
+  let states = [
+    {name: 'Arizona', code: 'Arizona'},
+    {name: 'California', value: 'California'},
+    {name: 'Florida', code: 'Florida'},
+    {name: 'Ohio', code: 'Ohio'},
+    {name: 'Washington', code: 'Washington'}
+  ]
+  const onStateChange = (e:any) => {
+    setSelectedState(e.value);
+  }
     return (
       <div className="createAPI">
         <h5>createAPI Page</h5>
+        <div className="p-grid">
+        <div className="p-col" />
+        <div className="p-col-10" >
+          <div className="p-fluid p-formgrid p-grid">
+          
+            <div className="p-field p-col-12 p-md-2">
+                <label htmlFor="state">State</label>
+                <Dropdown inputId="state" value={selectedState} options={states} onChange={onStateChange} placeholder="Select" optionLabel="name"/>
+            </div>
+            
+          </div>
+        </div>
+        <div className="p-col" />
+        </div>
       </div>
     )
   }
