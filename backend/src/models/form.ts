2c91b9a2af2f108c4b87e77c3b81dd9ae3105294
yubintw <yubin.cs05g@nctu.edu.tw>
2021-06-11 03:44:49 +0800

    Merge refactor-with-fp-ts and fix conflict

--- backend/src/models/form.ts
+++ backend/src/models/form.ts
@@ -1,12 +1,21 @@
 import mongoose from 'mongoose'
+import { IForm } from '../types/form'
 
 const Schema = mongoose.Schema
 
-const formSchema = new Schema({
-  apiId: { type: String, required: true },
-  subscriberId: { type: String, required: true },
-  submitUser: { type: String, required: true },
-  status: { type: String, required: true }
-})
+const formSchema = new Schema(
+  {
+    apiId: { type: String, required: true },
+    subscriberId: { type: String, required: true },
+    submitUser: { type: String, required: true },
+    status: { type: String, required: true },
+    approver: { type: String },
+    approveDate: { type: Date },
+    comment: { type: String }
+  },
+  {
+    timestamps: true
+  }
+)
 
-export default mongoose.model('Form', formSchema)
+export default mongoose.model<IForm>('Form', formSchema)
